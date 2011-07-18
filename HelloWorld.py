d318cffc2bc1d310cfd754a197c5194e4de2dc8a
Saad Mahamood <saadino@mac.com>
2011-07-18 06:37:56 +0100

    Added HelloWorld.py test file

---
+++ HelloWorld.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+HelloWorld.py
+
+Created by Saad Mahamood on 2011-07-18.
+Copyright (c) 2011. All rights reserved.
+"""
+
+import sys
+import os
+
+
+def main():
+	print "Hello World!"
+	pass
+
+
+if __name__ == '__main__':
+	main()
+
